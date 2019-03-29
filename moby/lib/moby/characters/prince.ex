294b247341a390669a6fd5644390729d6e4e3617
Bruno Parga <brunoparga@gmail.com>
2019-03-29 00:23:34 -0300

    fix: correctly end game when Prince is played last

--- moby/lib/moby/characters/prince.ex
+++ moby/lib/moby/characters/prince.ex
@@ -3,12 +3,25 @@ defmodule Moby.Prince do
 
   @spec play(GameState.t(), String.t()) :: GameState.t()
   def play(game, target_player) do
-    player = Player.find_by_name(game, target_player)
+    player = Moby.Player.find_by_name(game, target_player)
     target_card = hd(player.current_cards)
-    [drawn_card | new_deck] = game.deck
 
     Action.execute(game, target_player, {Action, :play_card, [target_card]})
-    |> Action.execute(target_player, {Action, :draw_card, [drawn_card]})
-    |> Map.put(:deck, new_deck)
+    |> check_deck(target_player)
+  end
+
+  defp check_deck(game, target_player) do
+    case length(game.deck) do
+      0 ->
+        Action.execute(game, target_player, {Action, :draw_card, [game.removed_card]})
+        |> Map.put(game, :removed_card, nil)
+        |> Moby.Victory.round_over()
+
+      _ ->
+        [drawn_card | new_deck] = game.deck
+
+        Action.execute(game, target_player, {Action, :draw_card, [drawn_card]})
+        |> Map.put(:deck, new_deck)
+    end
   end
 end
