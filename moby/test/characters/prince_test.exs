0473c0101dcfa2af70db77628b80d0095a785525
Bruno Parga <brunoparga@gmail.com>
2019-04-04 22:50:07 -0300

    test: add test for Prince self-targeting

--- moby/test/characters/prince_test.exs
+++ moby/test/characters/prince_test.exs
@@ -4,7 +4,7 @@ defmodule Moby.PrinceTest do
   alias Moby.{Action, GameState, Player}
 
   describe "play/1" do
-    test "target another player" do
+    test "target another player to discard" do
       joe = %Player{name: "Joe", current_cards: [:prince, :king]}
       ann = %Player{name: "Ann", current_cards: [:guard]}
 
@@ -21,6 +21,26 @@ defmodule Moby.PrinceTest do
       expected_joe = %Player{joe | current_cards: [:king], played_cards: [:prince]}
       expected_ann = %Player{ann | current_cards: [:princess], played_cards: [:guard]}
       expected = %GameState{game | players: [expected_joe, expected_ann], deck: tl(game.deck)}
+
+      assert actual == expected
+    end
+
+    test "target self to discard" do
+      joe = %Player{name: "Joe", current_cards: [:prince, :king]}
+      ann = %Player{name: "Ann", current_cards: [:guard]}
+
+      move = %{played_card: :prince, target: "Joe"}
+
+      game =
+        %GameState{players: [joe, ann], deck: ~w[princess countess prince handmaid
+        handmaid baron baron priest guard guard guard guard]a, removed_card: :priest}
+        |> GameState.set_move(move)
+        |> Action.execute_current({Action, :play_card, [:prince]})
+
+      actual = Moby.Prince.play(game)
+
+      expected_joe = %Player{joe | current_cards: [:princess], played_cards: [:prince, :king]}
+      expected = %GameState{game | players: [expected_joe, ann], deck: tl(game.deck)}
 
       assert actual == expected
     end
