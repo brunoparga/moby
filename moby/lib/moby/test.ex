ac607e96c7f0e67dda4c37989c075d8da778004e
Bruno Parga <brunoparga@gmail.com>
2022-04-14 12:49:59 +0400

    Improve manual test module logic

--- moby/lib/moby/test.ex
+++ moby/lib/moby/test.ex
@@ -3,34 +3,56 @@ defmodule Moby.Test do
   Automate running the game.
   """
 
+  @valid_cards ~w[baron countess guard handmaid king priest prince princess]a
+  @nontargeted_cards ~w[princess countess handmaid]a
+  @targeted_cards ~w[king prince baron priest]a
+
   @doc """
   Starts a game and keeps prompting the player for a move, which is executed.
   """
   def test do
     Moby.new_game(["Joe", "Ann"])
-    |> make_move()
+    |> play_game()
   end
 
-  defp make_move(pid) do
-    Moby.game_state(pid)
+  defp play_game(game) do
+    Moby.game_state(game)
     |> IO.inspect()
 
-    card = IO.gets("Choose a card: ") |> String.trim() |> String.to_atom()
+    card = get_card("Choose a card: ")
 
     cond do
-      card in ~w[princess countess handmaid]a ->
-        Moby.make_move(pid, %{played_card: card})
-
-      card in ~w[king prince baron priest]a ->
-        target = IO.gets("Choose target player: ") |> String.trim()
-        Moby.make_move(pid, %{played_card: card, target: target})
-
       card == :guard ->
-        target = IO.gets("Choose target player: ") |> String.trim()
-        named_card = IO.gets("Choose a non-Guard card: ") |> String.trim() |> String.to_atom()
-        Moby.make_move(pid, %{played_card: :guard, target: target, named_card: named_card})
+        target = get_target()
+        play_guard(game, target)
+
+      card in @targeted_cards ->
+        Moby.make_move(game, %{played_card: card, target: get_target()})
+
+      card in @nontargeted_cards ->
+        Moby.make_move(game, %{played_card: card})
     end
 
-    make_move(pid)
+    play_game(game)
+  end
+
+  defp play_guard(game, target) do
+    named_card = get_card("Choose a non-Guard card: ")
+
+    if named_card == :guard do
+      IO.puts("You cannot guess the Guard. Choose another card: ")
+      play_guard(game, target)
+    else
+      Moby.make_move(game, %{played_card: :guard, target: target, named_card: named_card})
+    end
+  end
+
+  defp get_target() do
+    IO.gets("Choose target player: ") |> String.trim()
+  end
+
+  defp get_card(prompt) do
+    card = IO.gets(prompt) |> String.trim() |> String.to_atom()
+    if card in @valid_cards, do: card, else: get_card("Invalid card. " <> prompt)
   end
 end
